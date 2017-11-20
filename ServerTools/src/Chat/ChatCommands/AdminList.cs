76f28c16cf9efd2cfc0cc7a1c58ddeeb2b1a3588
ObsessiveCompulsive <crobmail@hotmail.com>
2017-11-19 21:17:19 -0500

    5.55
    Fixed /admin chat command response

--- ServerTools/src/Chat/ChatCommands/AdminList.cs
+++ ServerTools/src/Chat/ChatCommands/AdminList.cs
@@ -17,28 +17,36 @@ namespace ServerTools
             List<ClientInfo> _cInfoList = ConnectionManager.Instance.GetClients();
             foreach (var _cInfoAdmins in _cInfoList)
             {
-                GameManager.Instance.adminTools.IsAdmin(_cInfoAdmins.playerId);
-                AdminToolsClientInfo Admin = GameManager.Instance.adminTools.GetAdminToolsClientInfo(_cInfoAdmins.playerId);
-                if (Admin.PermissionLevel <= AdminLevel)
+                if (!AdminChatColor.AdminColorOff.Contains(_cInfoAdmins.playerId))
                 {
-                    Admins.Add(_cInfoAdmins.playerName);
+                    GameManager.Instance.adminTools.IsAdmin(_cInfoAdmins.playerId);
+                    AdminToolsClientInfo Admin = GameManager.Instance.adminTools.GetAdminToolsClientInfo(_cInfoAdmins.playerId);
+                    if (Admin.PermissionLevel <= AdminLevel)
+                    {
+                        Admins.Add(_cInfoAdmins.playerName);
+                    }
+                    if (Admin.PermissionLevel > AdminLevel & Admin.PermissionLevel <= ModLevel)
+                    {
+                        Mods.Add(_cInfoAdmins.playerName);
+                    }
                 }
-                if (Admin.PermissionLevel > AdminLevel && Admin.PermissionLevel <= ModLevel)
-                {
-                    Mods.Add(_cInfoAdmins.playerName); 
-                }
-                string _adminList = string.Join(", ", Admins.ToArray());
-                string _modList = string.Join(", ", Mods.ToArray());
-                if (_announce)
-                {
-                    GameManager.Instance.GameMessageServer(_cInfo, EnumGameMessages.Chat, string.Format("{0}Server admins in game: [FF8000]{1}[-]", CustomCommands.ChatColor, _adminList), "Server", false, "", false);
-                    GameManager.Instance.GameMessageServer(_cInfo, EnumGameMessages.Chat, string.Format("{0}Server mods in game: [FF8000]{1}[-]", CustomCommands.ChatColor, _modList), "Server", false, "", false);
-                }
-                else
-                {
-                    _cInfo.SendPackage(new NetPackageGameMessage(EnumGameMessages.Chat, string.Format("{0}Server admins in game: [FF8000]{1}[-]", CustomCommands.ChatColor, _adminList), "Server", false, "", false));
-                    _cInfo.SendPackage(new NetPackageGameMessage(EnumGameMessages.Chat, string.Format("{0}Server mods in game: [FF8000]{1}[-]", CustomCommands.ChatColor, _modList), "Server", false, "", false));
-                }
+            }
+            Response(_cInfo, _announce, _playerName);
+        }
+
+        public static void Response(ClientInfo _cInfo, bool _announce, string _playerName)
+        {
+            string _adminList = string.Join(", ", Admins.ToArray());
+            string _modList = string.Join(", ", Mods.ToArray());
+            if (_announce)
+            {
+                GameManager.Instance.GameMessageServer(_cInfo, EnumGameMessages.Chat, string.Format("{0}Server admins in game: [FF8000]{1}[-]", CustomCommands.ChatColor, _adminList), "Server", false, "", false);
+                GameManager.Instance.GameMessageServer(_cInfo, EnumGameMessages.Chat, string.Format("{0}Server mods in game: [FF8000]{1}[-]", CustomCommands.ChatColor, _modList), "Server", false, "", false);
+            }
+            else
+            {
+                _cInfo.SendPackage(new NetPackageGameMessage(EnumGameMessages.Chat, string.Format("{0}Server admins in game: [FF8000]{1}[-]", CustomCommands.ChatColor, _adminList), "Server", false, "", false));
+                _cInfo.SendPackage(new NetPackageGameMessage(EnumGameMessages.Chat, string.Format("{0}Server mods in game: [FF8000]{1}[-]", CustomCommands.ChatColor, _modList), "Server", false, "", false));
             }
         }
     }
--- ServerTools/src/Functions/LoadConfig.cs
+++ ServerTools/src/Functions/LoadConfig.cs
@@ -8,7 +8,7 @@ namespace ServerTools
         private const string configFile = "ServerToolsConfig.xml";
         private static string configFilePath = string.Format("{0}/{1}", API.ConfigPath, configFile);
         private static FileSystemWatcher fileWatcher = new FileSystemWatcher(API.ConfigPath, configFile);
-        private const double version = 5.54;
+        private const double version = 5.55;
         public static bool UpdateConfigs = false;
 
         public static void Load()
